78e72bb301a14105bb3b59d31553d2d15a4f5d75
Lino Alves <lino.alves@liferay.com>
2017-10-05 19:00:40 -0300

    remote unzip specifc file

---
+++ src/main/java/com/remoteunzip/demo/MyUnzipRemoteFile.java
@@ -0,0 +1,151 @@
+package com.remoteunzip.demo;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.net.Authenticator;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.nio.file.Files;
+import java.util.Base64;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+
+@RestController
+@RequestMapping("/unzip")
+@EnableAutoConfiguration
+public class MyUnzipRemoteFile {
+
+		
+	@RequestMapping(value="/hotfix/{version}/{fixid}", method=RequestMethod.GET)
+	 ResponseEntity<Resource> getHotfixDoc(
+			 @RequestHeader(value="auth") String basicAuth,
+			 @PathVariable String version,@PathVariable String fixid) {
+	
+		String hotfixLink = "https://files.liferay.com/private/ee/fix-packs/" + version + "/hotfix/liferay-hotfix-" + fixid + ".zip";
+		
+		String fixDoc = "fixpack_documentation.xml";
+		
+		byte[] credentials = Base64.getDecoder().decode(basicAuth);
+		
+		try {
+
+			String[] credentialsArray = new String(credentials, "UTF-8").split(":");
+			
+			String username = credentialsArray[0];
+			
+			String password = credentialsArray[1];	
+				
+			prepareAuthenticator(username, password);
+			
+			File extracted = extractFile(fixDoc, hotfixLink);
+			
+			if(extracted != null) {
+				
+				ByteArrayResource resource = new ByteArrayResource(Files.readAllBytes(extracted.toPath()));
+
+			    return ResponseEntity.ok()
+			            .contentLength(extracted.length())
+			            .contentType(MediaType.parseMediaType("application/xml"/*"application/octet-stream"*/))
+			            .body(resource);
+				
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
+	
+	
+	public File extractFile(String fileToExtract, String zipUrl) 
+		throws IOException {
+		
+		if(fileToExtract == null || zipUrl == null)
+			return null;
+		
+		URL url = new URL(zipUrl);
+		
+		ZipInputStream zin = new ZipInputStream(url.openStream());
+		ZipEntry ze = zin.getNextEntry();
+		
+		while (!ze.getName().equals(fileToExtract)) {
+		    zin.closeEntry();
+		    ze = zin.getNextEntry();
+		}
+		
+		File tempFile = File.createTempFile(fileToExtract, "unziped");
+		
+		
+		FileOutputStream fos = new FileOutputStream(tempFile);
+		
+		int next = zin.read();
+
+		while(next != -1) {
+			
+			fos.write(next);
+			next = zin.read();
+			
+		} 
+		
+		fos.flush();
+		fos.close();
+		
+		return tempFile;
+	}
+
+
+	@Configuration
+	@EnableWebSecurity
+	public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		 
+		protected void configure(HttpSecurity http) throws Exception {
+			
+			 http
+             .authorizeRequests()
+             .anyRequest().permitAll();
+
+		}
+		
+		
+	   @Autowired
+	    public void configureGlobal(AuthenticationManagerBuilder auth)
+	            throws Exception {
+	        auth
+	            .inMemoryAuthentication()
+	                .withUser("user").password("password").roles("USER");
+	    	
+	    	
+	    }
+	
+	}
+	
+
+	private void prepareAuthenticator(String user, String password) {
+		Authenticator.setDefault (new Authenticator() {
+		    protected PasswordAuthentication getPasswordAuthentication() {
+		        return new PasswordAuthentication (user, password.toCharArray());
+		    }
+		});
+	}
+
+}
