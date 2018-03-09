3d8d9eb5b01da1706f42ec2db375d80370b2cae0
Lino Alves <lino.alves@liferay.com>
2018-03-09 16:33:06 -0300

    testing response content

--- src/test/java/com/remoteunzip/demo/MyUnzipRemoteFileTests.java
+++ src/test/java/com/remoteunzip/demo/MyUnzipRemoteFileTests.java
@@ -2,6 +2,9 @@ package com.remoteunzip.demo;
 
 import java.time.Duration;
 
+import org.assertj.core.api.Assertions;
+import org.json.JSONObject;
+import org.json.XML;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -17,7 +20,7 @@ import com.remoteunzip.demo.util.Credentials;
 public class MyUnzipRemoteFileTests {
 
 	private WebTestClient webClient = WebTestClient.bindToServer() 
-            .responseTimeout(Duration.ofMillis(30000)).build();
+            .responseTimeout(Duration.ofMillis(10000)).build();
 		
 	
 	private ConfigProperties properties;
@@ -40,7 +43,7 @@ public class MyUnzipRemoteFileTests {
 	}
 	
 	@Test
-	public void getFixPackDocResponseStatusTest() {
+	public void getFixpackDocResponseStatusTest() {
 		Credentials credentials = new Credentials(properties.getUsername(), properties.getPassword());
 		
 		String basicAuth = 	credentials.getBasicAuth();
@@ -48,6 +51,54 @@ public class MyUnzipRemoteFileTests {
 		this.webClient.get()
 		.uri("http://localhost:8080/unzip/fixpack/6.2.10/164-6210")
 		.header("auth", basicAuth).exchange().expectStatus().isOk();
+		
+	}
+	
+	@Test
+	public void getFixpackDocResponseContentTest() {
+		Credentials credentials = new Credentials(properties.getUsername(), properties.getPassword());
+		
+		String basicAuth = 	credentials.getBasicAuth();
+		
+		this.webClient.get()
+		.uri("http://localhost:8080/unzip/fixpack/6.2.10/164-6210")
+		.header("auth", basicAuth).exchange().expectStatus().isOk().expectBody()
+		.consumeWith(response ->{
+			Assertions.assertThat(response.getResponseBody()).isNotNull();
+			
+			String body = new String(response.getResponseBody());
+			
+			JSONObject fixDocJson = XML.toJSONObject(body);
+			
+			JSONObject patchJson = fixDocJson.getJSONObject("patch");
+			
+			Assert.assertEquals("portal-164-6210", patchJson.getString("id"));
+			Assert.assertEquals(83011128, patchJson.getLong("build-id"));
+		});
+		
+	}
+	
+	@Test
+	public void getHotfixDocResponseContentTest() {
+		Credentials credentials = new Credentials(properties.getUsername(), properties.getPassword());
+		
+		String basicAuth = 	credentials.getBasicAuth();
+		
+		this.webClient.get()
+		.uri("http://localhost:8080/unzip/hotfix/7.0.10/1222-7010")
+		.header("auth", basicAuth).exchange().expectStatus().isOk().expectBody()
+		.consumeWith(response ->{
+			Assertions.assertThat(response.getResponseBody()).isNotNull();
+			
+			String body = new String(response.getResponseBody());
+			
+			JSONObject fixDocJson = XML.toJSONObject(body);
+			
+			JSONObject patchJson = fixDocJson.getJSONObject("patch");
+			
+			Assert.assertEquals("hotfix-1222-7010", patchJson.getString("id"));
+			Assert.assertEquals(76752134, patchJson.getLong("build-id"));
+		});
 		
 	}
 }
